2a01f82bb65a18ba6ceebaab50e364827a9c61f8
Zamechek, Shawn <zamechek@wharton.upenn.edu>
2016-12-14 14:40:37 -0500

    init

---
+++ mfnbc/__init__.py
@@ -0,0 +1,1 @@
+print("i am in init")
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+    name='mfnbc',
+    version='0.7',
+    license='The MIT License (MIT)',
+    author="Shawn",
+    author_email='shawnzam@gmail.com',
+    url='http://zamechek.com',
+    packages=['mfnbc'],
+    install_requires=['nltk>=3.2'],
+    keywords=['bayes'],
+)
